ee025b1c017049fd3a8f27c6a4c1ecf7c8003c00
lahlaliwassim2 <wlahlali34@gmail.com>
2022-08-05 14:41:47 +0100

    aouter les route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StaticController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [StaticController::class, 'index'] );
 
 
 // Route::get('/store', function () {
