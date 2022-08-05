53ba2326c3aea2b4d8c07d912640e9a6821273ec
lahlaliwassim2 <wlahlali34@gmail.com>
2022-08-05 20:13:12 +0100

    ajouter le controller StaticControlleur

--- app/Http/Controllers/StaticController.php
+++ app/Http/Controllers/StaticController.php
@@ -9,5 +9,11 @@ class StaticController extends Controller
     public function index() {
         return view('welcome');
     }
-    
+    public function about(){
+        return view('about');
+    }
+    public function contact(){
+        return view('contact');
+    }
+
 }
