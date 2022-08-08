e8ef05f6ec2621233fdf0313fce2a48936b63e7d
lahlaliwassim2 <wlahlali34@gmail.com>
2022-08-08 11:51:46 +0100

    ajouter la page de contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,1 +1,13 @@
-<h1>contact</h1>
+@extends('layout')
+@section('title', 'contact')
+@section('content')
+<div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+
+            <div class="flex justify-center pt-8">
+                <h1>contact</h1>
+            </div>
+            <div class="mt-8">
+                THIS IS THE CONTACT PAGE
+            </div>
+        </div>
+@endsection
