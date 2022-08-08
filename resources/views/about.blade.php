c6e79ac590b6cb6f7779ce6ace493bec6ba29b51
lahlaliwassim2 <wlahlali34@gmail.com>
2022-08-08 11:49:49 +0100

    ajouter la page about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,1 +1,13 @@
-<h1>abour</h1>
+@extends('layout')
+@section('title', 'about')
+@section('content')
+<div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+
+            <div class="flex justify-center pt-8">
+                <h1>ABOUT</h1>
+            </div>
+            <div class="mt-8">
+                THIS IS THE ABOUT PAGE
+            </div>
+        </div>
+@endsection
